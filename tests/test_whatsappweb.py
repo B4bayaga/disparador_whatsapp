397873e0fb085f0fb1c193140324f891b37af373
Rafael Oliveira <rafael@lyquidity.com.br>
2023-10-19 09:38:53 -0300

    Testes funcionando.

--- tests/test_whatsappweb.py
+++ tests/test_whatsappweb.py
@@ -11,6 +11,8 @@ def random_generator(size=12, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
 
+numero_cobranca = '77999255107'
+numero_rafael = '77992129494'
 @pytest.fixture
 def Wp():
     Wp = DisparadorApp()
@@ -31,23 +33,22 @@ def test_elemento_espera_apos_login(Wp):
     assert Wp.espera_login()
 
 
+@pytest.mark.slow
 def test_elemento_digita_numero_whatsapp(Wp):
     Wp.espera_login()
     Wp._DisparadorApp__abre_caixa_digita_telefone()
-    assert Wp.wait.until(EC.presence_of_element_located((
+    assert Wp.wait_curta.until(EC.presence_of_element_located((
         By.XPATH,
         Wp.caixa_digita_numero_celular
     )))
 
 
-@pytest.mark.slow
 def test_elemento_numero_sem_whatsapp(Wp):
     Wp.espera_login()
-    numero_rafael_cobranca = '77999255107'
-    Wp.digita_numero_telefone(numero_rafael_cobranca)
-    assert Wp.wait.until(EC.presence_of_element_located((
+    Wp.digita_numero_telefone(numero_cobranca)
+    assert Wp.wait_curta.until(EC.presence_of_element_located((
         By.XPATH,
-        Wp._DisparadorApp__elemento_caixa_digita_numero_sem_whatsapp
+        Wp.caixa_digita_numero_celular
     )))
 
 
@@ -60,8 +61,8 @@ def test_remove_9_telefone():
 
 def test_elemento_espera_caixa_digita_mensagem(Wp):
     Wp.espera_login()
-    Wp.digita_numero_telefone('77992129494')
-    assert Wp.wait.until(EC.presence_of_element_located((
+    Wp.digita_numero_telefone(numero_rafael)
+    assert Wp.wait_curta.until(EC.presence_of_element_located((
         By.XPATH,
         Wp.elemento_caixa_digita_mensagem
     )))
@@ -70,7 +71,7 @@ def test_elemento_espera_caixa_digita_mensagem(Wp):
 def test_texto_envia_mensagem(Wp):
     palavra_teste = random_generator()
     Wp.espera_login()
-    Wp.digita_numero_telefone('77992129494')
+    Wp.digita_numero_telefone(numero_rafael)
     Wp.envia_mensagem(palavra_teste)
     sleep(0.5)
     mensagens_enviada = Wp.driver.find_elements(
